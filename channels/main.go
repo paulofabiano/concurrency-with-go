06db9da4dcc0ebbedf3d6437ee44c833ef2c6646
Paulo Langer <paulo.langer@xing.com>
2020-04-24 16:51:44 +0100

    working with channel types: bidirecional, read-only and write-only

--- channels/main.go
+++ channels/main.go
@@ -10,14 +10,12 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
-		fmt.Println(<-ch) // if I don't have this, just the first value will be shown
 		wg.Done()
 	}(ch, wg)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
-		ch <- 27
 		wg.Done()
 	}(ch, wg)
 
