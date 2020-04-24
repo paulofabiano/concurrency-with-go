9714bbbba6d954dcfff8dd5c9b75c204ca62203f
Paulo Langer <paulo.langer@xing.com>
2020-04-24 18:00:55 +0100

    using channels and select statement on course demo

--- course-demo/main.go
+++ course-demo/main.go
@@ -13,25 +13,41 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
 
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		}(id, wg, m, cacheCh)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			if b, ok := queryDatabase(id, m); ok {
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbCh)
+
+		// create one goroutine per query to handle response
+		go func(cacheCh, dbCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(b)
+				<-dbCh
+			case b := <-dbCh:
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-			wg.Done()
-		}(id, wg, m)
+		}(cacheCh, dbCh)
 
 		time.Sleep(150 * time.Millisecond)
 	}
