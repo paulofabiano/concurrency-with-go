44fd9efb4762f6478a8d4bc76f372d7eca58a7e7
Paulo Langer <paulo.langer@xing.com>
2020-04-24 17:01:48 +0100

    closing channels flow control with if statement

--- channels/main.go
+++ channels/main.go
@@ -11,11 +11,14 @@ func main() {
 
 	wg.Add(2)
 	go func(ch <-chan int, wg *sync.WaitGroup) {
-		fmt.Println(<-ch)
+		if msg, ok := <-ch; ok {
+			fmt.Println(msg, ok)
+		}
 		wg.Done()
 	}(ch, wg)
 	go func(ch chan<- int, wg *sync.WaitGroup) {
-		ch <- 42
+		// ch <- 0
+		close(ch)
 		wg.Done()
 	}(ch, wg)
 
