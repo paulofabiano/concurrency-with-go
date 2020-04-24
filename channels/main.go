04186d2d80cdc6688a2f1a81e8ea9c0e1160da0a
Paulo Langer <paulo.langer@xing.com>
2020-04-24 16:46:19 +0100

    using buffered channel

--- channels/main.go
+++ channels/main.go
@@ -7,15 +7,17 @@ import (
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	ch := make(chan int, 1)
 
 	wg.Add(2)
 	go func(ch chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
+		fmt.Println(<-ch) // if I don't have this, just the first value will be shown
 		wg.Done()
 	}(ch, wg)
 	go func(ch chan int, wg *sync.WaitGroup) {
 		ch <- 42
+		ch <- 27
 		wg.Done()
 	}(ch, wg)
 
